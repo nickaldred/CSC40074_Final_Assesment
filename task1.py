708d7960f2dae9e3d6600f11344dcbbc3b00ee6e
nickaldred <nickaldred@hotmail.co.uk>
2022-04-21 18:04:58 +0100

    Added convert from numbers to letter method

--- task1.py
+++ task1.py
@@ -21,6 +21,8 @@ class Graph:
 
     # Add edges
     def add_edge(self, s, d, weight):
+        s = self.convert_letter(s)
+        d = self.convert_letter(d)
         node = AdjNode(d, weight)
         node.next = self.graph[s]
         self.graph[s] = node
@@ -32,35 +34,31 @@ class Graph:
     # Print the graph
     def print_agraph(self):
         for i in range(self.V):
-            print("Vertex " + str(i) + ":", end="")
+            print("Vertex " + self.convert_no(i) + ":", end="")
             temp = self.graph[i]
             while temp:
-                print(f" -> {temp.vertex}, {temp.weight}", end="")
+                print(f" -> {self.convert_no(temp.vertex)}, w:{temp.weight}", end="")
 
                 temp = temp.next
             print(" \n")
 
+    def convert_no(self, num): 
+        return(chr(num+97))
+    
+    def convert_letter(self,letter):
+        return(ord(letter)-97)
+
+
 
 if __name__ == "__main__":
-    V = 5
+    V = 4
 
     # Create graph and edges
-
-    #a = 1
-    #b = 2
-    #c = 3
-    #d = 4
-    #e = 5
-    #f = 6
-    #g = 7
-    #h = 8
-    #i = 9
-    #j = 10
-    #k = 11
+    
     graph = Graph(V)
-    graph.add_edge(0, 1, 3)
-    graph.add_edge(0, 2, 4)
-    graph.add_edge(0, 3, 2)
-    graph.add_edge(1, 2, 1)
+    graph.add_edge('a', 'b', 3)
+    graph.add_edge('a', 'c', 4)
+    graph.add_edge('a', 'd', 2)
+    graph.add_edge('b', 'c', 1)
 
     graph.print_agraph()
